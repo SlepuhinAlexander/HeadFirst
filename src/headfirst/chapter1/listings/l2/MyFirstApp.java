730fdb63e237cb2e142d8ff019a550a50ee9fd51
Superuser <Superuser@gmail.com>
2020-02-02 18:12:43 +0300

    Added Chapter 1 Listing 2

---
+++ src/headfirst/chapter1/listings/l2/MyFirstApp.java
@@ -0,0 +1,8 @@
+package headfirst.chapter1.listings.l2;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.print("I can rule ");
+        System.out.println("the World!");
+    }
+}
