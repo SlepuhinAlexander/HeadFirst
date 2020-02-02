e41875ae3b6c5a7024977542530dbcc6ed47d86a
Superuser <Superuser@gmail.com>
2020-02-02 22:13:20 +0300

    Added Chapter 1 Listing 1

---
+++ src/headfirst/chapter2/listings/l1/Dog.java
@@ -0,0 +1,11 @@
+package headfirst.chapter2.listings.l1;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Woof! Woof!");
+    }
+}
---
+++ src/headfirst/chapter2/listings/l1/DogTestDrive.java
@@ -0,0 +1,9 @@
+package headfirst.chapter2.listings.l1;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
