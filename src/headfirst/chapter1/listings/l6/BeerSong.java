b38a5577abe7529a327a6e7a5b27eaf5bef3e291
Superuser <Superuser@gmail.com>
2020-02-02 18:51:10 +0300

    Added Chapter 1 Listing 6

---
+++ src/headfirst/chapter1/listings/l6/BeerSong.java
@@ -0,0 +1,24 @@
+package headfirst.chapter1.listings.l6;
+
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+        while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+            System.out.println(beerNum + " " + word + " of beer on the wall.");
+            System.out.println(beerNum + " " + word + " of beer on the wall.");
+            System.out.println("Get one more.");
+            System.out.println("Run it around.");
+            beerNum = beerNum - 1;
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall.");
+                System.out.println();
+            } else {
+                System.out.println("No more beer on the wall");
+            }
+        }
+    }
+}
