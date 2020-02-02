f44ba4711f1e91699c3de633d363ca6b79411760
Superuser <Superuser@gmail.com>
2020-02-03 01:25:46 +0300

    Added Chapter 4 Listing 4

---
+++ src/headfirst/chapter4/listings/l4/PoorDog.java
@@ -0,0 +1,14 @@
+package headfirst.chapter4.listings.l4;
+
+public class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
