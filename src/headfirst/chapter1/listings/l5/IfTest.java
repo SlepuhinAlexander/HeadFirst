8bf68ea8c73c1d5e995e6deeb5ec43b951fbbb86
Superuser <Superuser@gmail.com>
2020-02-02 18:35:11 +0300

    Added Chapter 1 Listing 5

---
+++ src/headfirst/chapter1/listings/l5/IfTest.java
@@ -0,0 +1,11 @@
+package headfirst.chapter1.listings.l5;
+
+public class IfTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x must be equal to 3");
+        }
+        System.out.println("This line is executed in any case");
+    }
+}
