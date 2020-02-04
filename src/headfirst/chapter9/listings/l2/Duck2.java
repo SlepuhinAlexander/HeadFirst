696007937aa37178a7f476571f049571b52adbce
Superuser <Superuser@gmail.com>
2020-02-04 22:18:56 +0300

    Added Chapter 9 Listing 2

---
+++ src/headfirst/chapter9/listings/l2/Duck2.java
@@ -0,0 +1,13 @@
+package headfirst.chapter9.listings.l2;
+
+public class Duck2 {
+    int size;
+
+    public Duck2() {
+        size = 27;
+    }
+
+    public Duck2(int duckSize) {
+        size = duckSize;
+    }
+}
