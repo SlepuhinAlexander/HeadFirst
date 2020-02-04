9d0da9f13cdcea9c64e842efb271c7315721377c
Superuser <Superuser@gmail.com>
2020-02-04 22:12:47 +0300

    Added Chapter 9 Listing 1

---
+++ src/headfirst/chapter9/listings/l1/Duck.java
@@ -0,0 +1,7 @@
+package headfirst.chapter9.listings.l1;
+
+public class Duck {
+    public Duck() {
+        System.out.println("Quack");
+    }
+}
---
+++ src/headfirst/chapter9/listings/l1/UseADuck.java
@@ -0,0 +1,13 @@
+package headfirst.chapter9.listings.l1;
+
+public class UseADuck {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+    }
+}
+
+/*
+* output:
+* % java UseADuck
+* Quack
+* */
