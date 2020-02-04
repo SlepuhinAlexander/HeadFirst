cdb9e64a176aa0814ff2cb40dc6683ee57522756
Superuser <Superuser@gmail.com>
2020-02-04 19:58:35 +0300

    Improved Chapter 8 Listing 2 with add / remove methods and their calls

--- src/headfirst/chapter8/listings/l2/MyDogList.java
+++ src/headfirst/chapter8/listings/l2/MyDogList.java
@@ -5,20 +5,44 @@ import headfirst.chapter8.listings.l1.Dog;
 public class MyDogList {
     public static void main(String[] args) {
         MyDogList list = new MyDogList();
-        Dog a = new Dog();
-        Dog b = new Dog();
-        list.add(a);
-        list.add(b);
+        list.add(new Dog());
+        list.add(new Dog());
+        list.add(new Dog());
+        list.add(new Dog());
+        list.remove(1);
+        list.add(new Dog());
+        list.add(new Dog());
+        list.add(new Dog());
     }
 
     private Dog[] dogs = new Dog[5];
-    private int nextIndex = 0;
 
-    public void add(Dog d) {
-        if (nextIndex < dogs.length) {
+    public boolean add(Dog d) {
+        int nextIndex = findFreeIndex();
+        if (nextIndex >= 0) {
             dogs[nextIndex] = d;
             System.out.println("Dog added to cell " + nextIndex);
-            nextIndex++;
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public boolean remove(int index) {
+        if (index >= 0 && index < dogs.length) {
+            dogs[index] = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int findFreeIndex() {
+        for (int i = 0; i < dogs.length; i++) {
+            if (dogs[i] == null) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
