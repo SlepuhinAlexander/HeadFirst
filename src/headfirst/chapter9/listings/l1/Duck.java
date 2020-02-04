df7569bb1aa48108496fee3a2e696385453902c1
Superuser <Superuser@gmail.com>
2020-02-04 22:15:07 +0300

    Updated Chapter 9 Listing 1

--- src/headfirst/chapter9/listings/l1/Duck.java
+++ src/headfirst/chapter9/listings/l1/Duck.java
@@ -1,7 +1,13 @@
 package headfirst.chapter9.listings.l1;
 
 public class Duck {
+    int size;
+
     public Duck() {
         System.out.println("Quack");
+    }
+
+    public void setSize(int newSize) {
+        size = newSize;
     }
 }
--- src/headfirst/chapter9/listings/l1/UseADuck.java
+++ src/headfirst/chapter9/listings/l1/UseADuck.java
@@ -3,6 +3,8 @@ package headfirst.chapter9.listings.l1;
 public class UseADuck {
     public static void main(String[] args) {
         Duck d = new Duck();
+        /*Here the size of the Duck d is not initialized (equals 0).*/
+        d.setSize(42);
     }
 }
 
