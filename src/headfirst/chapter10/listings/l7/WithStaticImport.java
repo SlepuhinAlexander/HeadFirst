5662db0421601f52dba9f03cf40fd609b31c7643
Superuser <Superuser@gmail.com>
2020-02-05 19:39:02 +0300

    Added Chapter 10 Listing 7

---
+++ src/headfirst/chapter10/listings/l7/WithStaticImport.java
@@ -0,0 +1,16 @@
+package headfirst.chapter10.listings.l7;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class WithStaticImport {
+    public static void main(String[] args) {
+        out.println("sqrt " + sqrt(2.0));
+        out.println("tan " + tan(60));
+    }
+}
+/*
+ * output
+ * sqrt 1.4142135623730951
+ * tan 0.320040389379563
+ * */
