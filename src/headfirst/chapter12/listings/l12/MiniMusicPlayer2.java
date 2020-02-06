43794ec31a7f7146df224b8dd9e755d175fcab99
Superuser <Superuser@gmail.com>
2020-02-06 20:10:59 +0300

    Added Chapter 12 Listing 12

--- src/headfirst/chapter12/listings/l12/MiniMusicPlayer2.java
+++ src/headfirst/chapter12/listings/l12/MiniMusicPlayer2.java
@@ -1,15 +1,25 @@
-package headfirst.chapter12.listings.l11;
+package headfirst.chapter12.listings.l12;
 
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer1 {
+public class MiniMusicPlayer2 implements ControllerEventListener {
     public static void main(String[] args) {
-        try (Sequencer sequencer = MidiSystem.getSequencer()) {
+        MiniMusicPlayer2 mini = new MiniMusicPlayer2();
+        mini.go();
+    }
+
+    public void go() {
+        try (Sequencer sequencer = MidiSystem.getSequencer()){
             sequencer.open();
+
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this,eventsIWant);
+
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
-            for (int i = 5; i < 61; i += 4) {
+            for (int i = 5; i < 60; i += 4) {
                 track.add(makeEvent(ShortMessage.NOTE_ON, 1, i, 100, i));
+                track.add(makeEvent(ShortMessage.CONTROL_CHANGE, 1, 127, 0, i));
                 track.add(makeEvent(ShortMessage.NOTE_OFF, 1, i, 100, i + 2));
             }
             sequencer.setSequence(seq);
@@ -19,6 +29,11 @@ public class MiniMusicPlayer1 {
         } catch (MidiUnavailableException | InvalidMidiDataException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void controlChange(ShortMessage shortMessage) {
+        System.out.print("la");
     }
 
     public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
