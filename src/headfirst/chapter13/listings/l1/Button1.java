5487ce1f1685ce186e291c056b28d28a997e6dda
Superuser <Superuser@gmail.com>
2020-02-10 16:03:10 +0300

    Added Chapter 13 Listing 1

---
+++ src/headfirst/chapter13/listings/l1/Button1.java
@@ -0,0 +1,20 @@
+package headfirst.chapter13.listings.l1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button1 {
+    public static void main(String[] args) {
+        Button1 gui = new Button1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.EAST, button);
+        frame.setBounds(800, 400, 300, 300);
+        frame.setVisible(true);
+    }
+}
