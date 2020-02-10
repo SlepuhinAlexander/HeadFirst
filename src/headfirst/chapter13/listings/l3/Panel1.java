ee8869124a56c8cad1241fb22b47c33071cee6b1
Superuser <Superuser@gmail.com>
2020-02-10 16:31:54 +0300

    Updated Chapter 13 Listing 3 changed layout to vertical

--- src/headfirst/chapter13/listings/l3/Panel1.java
+++ src/headfirst/chapter13/listings/l3/Panel1.java
@@ -14,11 +14,12 @@ public class Panel1 {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setBackground(Color.DARK_GRAY);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         JButton button = new JButton("shock me");
         JButton buttonTwo = new JButton("bliss");
         panel.add(button);
         panel.add(buttonTwo);
-        frame.getContentPane().add(BorderLayout.EAST,panel);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
         frame.setBounds(800, 400, 300, 300);
         frame.setVisible(true);
     }
