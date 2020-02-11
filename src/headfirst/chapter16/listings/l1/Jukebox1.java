dff6671ade0b26d308c3e5f4f173ca397a5a78e3
Superuser <Superuser@gmail.com>
2020-02-12 00:42:13 +0300

    Added Chapter 16 Listing 1

---
+++ src/headfirst/chapter16/listings/l1/Jukebox1.java
@@ -0,0 +1,36 @@
+package headfirst.chapter16.listings.l1;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Jukebox1 {
+    public static final String PATH = System.getProperty("java.class.path") + "/" +
+            Jukebox1.class.getPackageName().replace('.', '/') + "/SongList.txt";
+    private ArrayList<String> songList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(PATH)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
+}
