026933e7224d9650cbc2aa32cf632b3af3eec15d
Superuser <Superuser@gmail.com>
2020-02-11 22:32:16 +0300

    Updated Chapter 15 Listing 3 with thread sleeps

--- src/headfirst/chapter15/listings/l3/MyRunnable.java
+++ src/headfirst/chapter15/listings/l3/MyRunnable.java
@@ -7,6 +7,11 @@ public class MyRunnable implements Runnable {
     }
 
     public void go() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         doMore();
     }
 
