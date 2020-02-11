7ae027c2d61687957ffe8e0839f3706441aa5c92
Superuser <Superuser@gmail.com>
2020-02-12 00:54:05 +0300

    Added Chapter 16 Listing 2

---
+++ src/headfirst/chapter16/listings/l2/Song.java
@@ -0,0 +1,41 @@
+package headfirst.chapter16.listings.l2;
+
+public class Song implements Comparable<Song>{
+    private String title;
+    private String artist;
+    private String rating;
+    private String bpm;
+
+    public Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(Song song) {
+        return title.compareTo(song.title);
+    }
+}
